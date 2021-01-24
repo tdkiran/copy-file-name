f0169bed2677ce5df263942f7d3b3ab55b48312b
Kiran Thirumalaichery Dhakshinamoorthy <kirandhakshina@gmail.com>
2021-01-23 22:50:47 -0600

    update cfn and cffn handler

--- src/extension.ts
+++ src/extension.ts
@@ -4,20 +4,35 @@ import * as path from 'path';
 export function activate(context: vscode.ExtensionContext) {
 
 	const cfn = vscode.commands.registerCommand('cfn', async () => {
-		if (!(vscode.window && typeof vscode.window.activeTextEditor !== 'function')) return false;
-		const currentEditor = await vscode.window.activeTextEditor;
-		const fileName = currentEditor ? currentEditor?.document.fileName : null;
-		if (!fileName) return false;
+
+		const oPath = vscode.window.activeTextEditor?.document?.fileName;
+
+		if (!oPath) {
+			vscode.window.showErrorMessage(`
+				cfn could be only executed only when file editor is active .
+			`);
+			return false;
+		}
+
+		const fileName = path.parse(oPath).name;
 		await vscode.env.clipboard.writeText(fileName);
 		vscode.window.showInformationMessage(`${fileName} copied!`);
 	});
 
 	const cffn = vscode.commands.registerCommand('cffn', async () => {
-		await vscode.commands.executeCommand('copyRelativeFilePath');
-		const rPath = await vscode.env.clipboard.readText();
-		const fileNameWithExtension = path.basename(rPath);
+		const oPath = vscode.window.activeTextEditor?.document?.fileName;
+
+		if (!oPath) {
+			vscode.window.showErrorMessage(`
+				cfn could be only executed only when file editor is active .
+			`);
+			return false;
+		}
+
+		const fileNameWithExtension = path.basename(oPath);
 		await vscode.env.clipboard.writeText(fileNameWithExtension);
 		vscode.window.showInformationMessage(`${fileNameWithExtension} copied!`);
+		console.log('kiran');
 	});
 
 	context.subscriptions.push(cffn);
